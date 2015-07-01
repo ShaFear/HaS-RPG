5dc6d28e8c9694914aeb9823966a456501652ec1
Michał Jereczek <michaljereczek@gmail.com>
2015-07-01 16:11:29 +0200

    dodanie SignUp activity

--- client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/LoginActivity.java
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.example.jereczem.hasrpg;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.jereczem.hasrpg.dialog.SimpleAlert;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -12,5 +19,37 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View view){
+        if(isLoginAndPasswordCorrect()){
+            //TODO [GET] domain.com/yourplay - sprawdza czy uzytkownik jest w aktywnej grze
+            if(true){
+                //TODO otworz LobbiesActivity
+            }
+            else{
+                //TODO otworz YourPlayActivity
+            }
+        }else{
+            new SimpleAlert(this,
+                    getString(R.string.wrong_login_or_password_title),
+                    getString(R.string.wrong_login_or_password_message));
+        }
+    }
+
+    public void signUp(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean isLoginAndPasswordCorrect() {
+        EditText loginEditText = (EditText)findViewById(R.id.login_input);
+        EditText passwordEditText = (EditText)findViewById(R.id.password_input);
+        String login = loginEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        //TODO [POST] domain.com/signin
+        if(login.equals("admin"))
+            return true;
+        return false;
     }
 }
---
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/SignUpActivity.java
@@ -0,0 +1,22 @@
+package com.example.jereczem.hasrpg;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class SignUpActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void signUp(View view){
+        this.finish();
+    }
+}
