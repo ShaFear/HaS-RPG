566b5dffe651b9e3ee7f450942840f125f534f2c
Michał Jereczek <michaljereczek@gmail.com>
2015-08-02 17:23:17 +0200

    dodanie toolbaru

--- client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/activities/LobbiesActivity.java
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/activities/LobbiesActivity.java
@@ -2,21 +2,27 @@ package com.example.jereczem.hasrpg.view.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.jereczem.hasrpg.R;
-import com.example.jereczem.hasrpg.view.fragments.LobbyFragment;
 import com.example.jereczem.hasrpg.view.logic.LobbiesActivityLogic;
 
 
-public class LobbiesActivity extends AppCompatActivity implements LobbyFragment.OnFragmentInteractionListener {
+public class LobbiesActivity extends AppCompatActivity{
 
     private LobbiesActivityLogic lobbiesActivityLogic;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobbies);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        toolbar.setNavigationIcon(R.drawable.list);
+        setSupportActionBar(toolbar);
         lobbiesActivityLogic = new LobbiesActivityLogic(this);
     }
 
@@ -32,12 +38,23 @@ public class LobbiesActivity extends AppCompatActivity implements LobbyFragment.
     }
 
     @Override
-    public void onFragmentInteraction(String id) {
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    public void selectCharacters(View view) {
-        lobbiesActivityLogic.selectCharactersClick();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                return true;
+            }
+            case R.id.action_create_new_lobby:  {
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void createNewLobby(View view) {
