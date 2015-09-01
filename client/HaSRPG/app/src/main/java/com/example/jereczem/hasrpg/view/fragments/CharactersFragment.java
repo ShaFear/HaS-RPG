be5147e0b57688f6253e4e1fa372bc258ee45bd1
Michał Jereczek <michaljereczek@gmail.com>
2015-09-01 10:13:23 +0200

    dodanie oblusgi guzikow zmiany postaci

--- client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/fragments/CharactersFragment.java
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/fragments/CharactersFragment.java
@@ -3,12 +3,10 @@ package com.example.jereczem.hasrpg.view.fragments;
 
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.jereczem.hasrpg.R;
 import com.example.jereczem.hasrpg.data.PlayerData;
@@ -33,7 +31,39 @@ public class CharactersFragment extends Fragment {
                              Bundle savedInstanceState) {
         activityView = inflater.inflate(R.layout.fragment_characters, container, false);
         characterSelectLogic = new CharacterSelectLogic(activityView, playerData);
+        setButtonListeners();
         return activityView;
+    }
+
+    private void setButtonListeners(){
+        Button leftChaseButton = (Button) activityView.findViewById(R.id.chaseLeftButton);
+        leftChaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                characterSelectLogic.leftChaseClick();
+            }
+        });
+        Button rightChaseButton = (Button) activityView.findViewById(R.id.chaseRightButton);
+        rightChaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                characterSelectLogic.rightChaseClick();
+            }
+        });
+        Button leftHunterButton = (Button) activityView.findViewById(R.id.hunterLeftButton);
+        leftHunterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                characterSelectLogic.leftHunterClick();
+            }
+        });
+        Button rightHunterButton = (Button) activityView.findViewById(R.id.hunterRightButton);
+        rightHunterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                characterSelectLogic.rightHunterClick();
+            }
+        });
     }
 
 }
