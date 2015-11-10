eb234f6822e459283101decedfc9b51327478ba4
Michał Jereczek <michaljereczek@gmail.com>
2015-11-10 11:32:20 +0100

    porządki

--- server/database/mysql/database.sql
+++
@@ -1,9 +0,0 @@
-source ./users.sql
-source ./characters.sql
-source ./lobbies.sql
-source ./functions/add_user.sql
-
-
-SELECT add_user('Shafear', '74c61aafb526b93a9e75690d70c48404cb4c90d079d79b3f7063dd3ef9949abf');
-SELECT add_user('Threexe', '74c61aafb526b93a9e75690d70c48404cb4c90d079d79b3f7063dd3ef9949abf');
-SELECT add_user('Test2', '74c61aafb526b93a9e75690d70c48404cb4c90d079d79b3f7063dd3ef9949abf');
--- server/database/mysql/users.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users
-(
-UserID 	      int 		      NOT NULL 	AUTO_INCREMENT 	PRIMARY KEY,
-login	        varchar(255) 	NOT NULL 	UNIQUE,
-passwd	      varchar(255)	NOT NULL,
-ChaseID       int          NOT NULL,
-HunterID      int          NOT NULL
-);
