fd9935ee910c02ca780bcdb601495a20b1d59606
Michał Jereczek <michaljereczek@gmail.com>
2015-11-24 07:56:13 +0100

    dodanie mozliwosci wyboru serwera

--- client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/settings/ServerSettings.java
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/settings/ServerSettings.java
@@ -4,8 +4,8 @@ package com.example.jereczem.hasrpg.settings;
  * Created by jereczem on 05.07.15.
  */
 public class ServerSettings {
-    public static final String SERVER_URL = "http://192.168.1.128:8080/";
-    public static final String SOCKET_SERVER_URL = "http://192.168.1.128:3000/";
+    public static String SERVER_URL = "http://192.168.1.128:8080/";
+    public static String SOCKET_SERVER_URL = "http://192.168.1.128:3000/";
     public static final Integer SOCKET_REJOINING_TIME = 3000; //miliseconds
     public static final String SOCKET_EVENT = "event";
     //public static final String SERVER_URL = "http://192.168.43.128/";
--- client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/activities/SignInActivity.java
+++ client/HaSRPG/app/src/main/java/com/example/jereczem/hasrpg/view/activities/SignInActivity.java
@@ -3,8 +3,10 @@ package com.example.jereczem.hasrpg.view.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.jereczem.hasrpg.R;
+import com.example.jereczem.hasrpg.settings.ServerSettings;
 import com.example.jereczem.hasrpg.view.logic.SignInLogic;
 
 
@@ -25,5 +27,17 @@ public class SignInActivity extends AppCompatActivity {
 
     public void signUp(View view) {
         signInLogic.signUpClick();
+    }
+
+    public void clickServer1(View view) {
+        ServerSettings.SERVER_URL = "http://192.168.1.128:8080/";
+        ServerSettings.SOCKET_SERVER_URL = "http://192.168.1.128:3000/";
+        Toast.makeText(this, "Settings changed to server 1", Toast.LENGTH_SHORT).show();
+    }
+
+    public voied clickServer2(View view) {
+        ServerSettings.SERVER_URL = "http://192.168.43.128:8080/";
+        ServerSettings.SOCKET_SERVER_URL = "http://192.168.43.128:3000/";
+        Toast.makeText(this, "Settings changed to server 2", Toast.LENGTH_SHORT).show();
     }
 }
