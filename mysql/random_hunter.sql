0d1646e38a695e8c0d3e2bd1163807b073e6ea1e
Michał <shafear@vps235120.ovh.net>
2016-01-10 15:50:17 +0100

    zmiana random

--- mysql/random_hunter.sql
+++ mysql/random_hunter.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `random_hunter`(ID int) RETURNS int(11)
+create FUNCTION random_hunter(ID int) RETURNS int(11)
 BEGIN
 set @random_hunter = (select * from (select users_login from lobbies l 
                     join lobbies_users lu on lu.lobbies_LobbyID=l.LobbyID 
