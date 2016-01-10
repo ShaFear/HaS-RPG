653e7b81fb1e7c39afc2d996c7e734d6e1502fce
trzye <shafear100@gmail.com>
2016-01-10 15:43:10 +0100

    ff

--- mysql/add_user.sql
+++ mysql/add_user.sql
@@ -2,16 +2,16 @@ CREATE FUNCTION add_user(log TEXT, pas TEXT)
 RETURNS INTEGER
 BEGIN
 	insert into users(login, passwd) values(log, pas);
-	insert into characters(role, users_UserID) values('hunter',(select UserID from users where login='Shafear'));
-	insert into characters(role, users_UserID) values('hunter',(select UserID from users where login='Shafear'));
-	insert into characters(role, users_UserID) values('chase',(select UserID from users where login='Shafear'));
-	insert into characters(role, users_UserID) values('chase',(select UserID from users where login='Shafear'));
+	insert into characters(role, users_login) values('hunter', log);
+	insert into characters(role, users_login) values('hunter', log);
+	insert into characters(role, users_login) values('chase', log);
+	insert into characters(role, users_login) values('chase', log);
 	update users u
-		join characters c on c.users_UserID = u.UserID
+		join characters c on c.users_login = u.login
 		set u.ChaseID=c.CharacterID
 		where c.role='chase' and u.login=log;
 	update users u
-		join characters c on c.users_UserID = u.UserID
+		join characters c on c.users_login = u.login
 		set u.HunterID=c.CharacterID
 		where c.role='hunter' and u.login=log;
 RETURN 1;
